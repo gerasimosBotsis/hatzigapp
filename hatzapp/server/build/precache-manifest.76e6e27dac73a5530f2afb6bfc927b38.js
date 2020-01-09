3723da262d64443802aadf3a92c60a6da4868f47
gerasimosBotsis <46564133+gerasimosBotsis@users.noreply.github.com>
2020-01-09 20:59:06 +0200

    Add files via upload

---
+++ hatzapp/server/build/precache-manifest.76e6e27dac73a5530f2afb6bfc927b38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da075f2afa3b3c6cf8c59562bfcaf82e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1141f3723537621eb693",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cc8db434ad44947392d3",
+    "url": "/static/js/2.ed98714a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ed98714a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1141f3723537621eb693",
+    "url": "/static/js/main.99509c52.chunk.js"
+  },
+  {
+    "revision": "7cf4ec57e9da9b54e700",
+    "url": "/static/js/runtime-main.08efed19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
